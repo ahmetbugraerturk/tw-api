2db06ffbd06072aed919af18c6671846d157929d
ahmetbugraerturk <bugra20042010@gmail.com>
2024-09-08 23:11:14 +0300

    hint api key

--- app/api/tweets/route.ts
+++ app/api/tweets/route.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export async function GET(request: Request) {
     const url = 'https://api.twitter.com/2/tweets/search/recent'; // Example endpoint
     const query = 'Searc Query'; // Replace with your search query
-    const bearerToken = 'AAAAAAAAAAAAAAAAAAAAAEEfvwEAAAAAaPLi75fi8McOgB6e5ChtRbE8se8%3DWL05vIjyRwgWcEaIoFx4HtoklBks8ic7nysMdkd2drEBQYt6tA'; // Replace with your Bearer Token
+    const bearerToken = process.env.TWITTER_BEARER_TOKEN; // Replace with your Bearer Token
 
     try {
         const response = await axios.get(url, {
--- app/api/twitter.ts
+++
@@ -1,9 +0,0 @@
-import { TwitterApi } from "twitter-api-v2";
-// By default, client are created with the max right-level (Read+Write+DMs)
-const client = new TwitterApi('<YOUR-TWITTER-BEARER-TOKEN>');
-
-// Read+Write level
-export const rwClient = client.readWrite;
-
-// Read-only level
-const roClient = client.readOnly;
